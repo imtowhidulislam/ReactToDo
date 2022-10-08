dfcb0810f5073cfddf0f3ae57e5c01a90da991d1
Towhid <niamaaktar@gmail.com>
2022-10-08 12:30:56 +0600

    All changes added to the repo

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h2 className='bg-blue-300 font-bold text-center uppercase text-red-700'>This is Towhidul Islam</h2>
+    </div>
+  );
+}
+
+export default App;
