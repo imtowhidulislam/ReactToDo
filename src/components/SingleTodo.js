88786f4a6887746c722d6bc15aec71f9e911b6ff
towhid <niamaaktar@gmail.com>
2022-10-09 15:19:05 +0600

    Edittodo functionality added to the repo

--- src/components/SingleTodo.js
+++ src/components/SingleTodo.js
@@ -3,41 +3,47 @@ import {AiFillDelete, AiFillEdit} from 'react-icons/ai'
 
 const SingleTodo = () => {
     const [todo , setTodo] = useState("");
-    const [search , setSearch] = useState("");
+    const [search, setSearch] = useState("");
     const [mytodo, setMytodo] = useState([]);
-    const [edit, setEdit] = useState(null)
+    const [edit, setEdit] = useState(null);
+    const [edited , setEdited] = useState(0);
 
     const searchChange = (e) => {
         console.log('searching todo');
     }
     const todoChange = (e) =>{
-        const data = e.target.value;
-        console.log(data);
-        setTodo(data)
+        const {value} = e.target;
+        setTodo(value)
     }
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('this is submit button');
+        
+
+        if(edited){
+            const updateTodo = mytodo.map(currVal => currVal.id === edited ? (currVal = {todo, id:currVal.id}) : currVal)
+
+            setMytodo(updateTodo);
+            setTodo("")
+            setEdited(0)
+            return;
+        }
         if(todo) {
             const id = new Date().getTime().toString();
-            const newTodo = {todo, id};
-            console.log(newTodo);
+            const newTodo = {...todo,id};
             setMytodo([...mytodo ,newTodo])
-            setTodo("")
+            setTodo({makeTodo : ""})
         }
     }
     // ! Delete Todo:
     const deleteTodo = (id) => {
-        console.log(id);
         setMytodo(remTodo => remTodo.filter(toodo => toodo.id !== id))
     }
     // ? Edit Todo:
     const editTodo = (id) => {
-        console.log(id);
-       const a = mytodo.filter(to => to.id === id);
-       console.log(a);
-
-       setTodo()
+       const a = mytodo.find(to => to.id === id);
+       const {todo:edittodo , id: editId} = a
+        setEdited(editId)
+       setTodo(edittodo)
     }
     useEffect(() => {
         const localTodo = JSON.parse(localStorage.getItem('todo'))
@@ -45,13 +51,11 @@ const SingleTodo = () => {
             setMytodo(localTodo)
         }
     },[])
+
     useEffect(() => {
-        console.log(mytodo);
         localStorage.setItem('todo', JSON.stringify(mytodo));
     },[mytodo])
-    useEffect(() => {
-        console.log(todo);
-    },[todo])
+
   return (
     <div className='flex items-center justify-center flex-col mt-8'>
         <h2 className='uppercase text-4xl font-bold first-letter:text-cyan-600 mb-8'>My Todos</h2>
@@ -60,7 +64,7 @@ const SingleTodo = () => {
                 <input name="search" placeholder='search' className='bg-transparent p-2' value={search} onChange={searchChange}></input>
             </div>
             <div className='flex items-center justify-between bg-gray-200  mt-2 mb-8'>
-                <input name="todos" placeholder='eg:your todos' className='bg-transparent  p-2' value={todo} onChange={todoChange}></input>
+                <input name="todo" placeholder='eg:your todos' className='bg-transparent  p-2' value={todo} onChange={todoChange}></input>
                 <button onClick={handleSubmit} type="submit" className='bg-cyan-600 w-max py-2 px-4 uppercase font-bold cursor-pointer hover:bg-cyan-800 hover:text-gray-300'>add</button>
             </div>
         </form>
